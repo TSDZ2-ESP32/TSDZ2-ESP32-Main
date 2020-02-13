ad34355d04c976dc716c6672dfa7ff6a344bbdc7
mspider65 <mspider65@aim.com>
2020-02-13 14:02:36 +0100

    I2C bus pin fix

--- main/tsdz_tmp112.h
+++ main/tsdz_tmp112.h
@@ -6,8 +6,8 @@
  */
 
 #define TPM112_ADDR		0x49
-#define SDA_PIN			11
-#define SCL_PIN			12
+#define SDA_PIN			13
+#define SCL_PIN			15
 
 void tsdz_tmp112_init(void);
 void tsdz_tmp112_read(void);
